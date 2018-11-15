3543e3d8a0e196eaa971285d41a8374efbd58ecc
enguyen93 <enguyenusaf93@gmail.com>
2018-11-15 12:59:57 -0600

    Fixed CORB

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,35 @@
-var friendsArray = [];
+var friendsArray = [
+    {
+        name: 'Delilah',
+        photo: 'https://vignette.wikia.nocookie.net/animal-jam-clans-1/images/7/70/5efdfcc02bcd058d5fc1feebce061c19--very-pretty-girl-pretty-girls.jpg/revision/latest?cb=20180802182137',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ]
+    }, {
+        name: 'Mia',
+        photo: 'https://m.media-amazon.com/images/M/MV5BMzc1YTA1ZjItMWRhMy00ZTBlLTkzNTgtNTc4ZDE3YTM3ZDk2XkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SY1000_SX1000_AL_.jpg',
+        scores: [
+            '1',
+            '3',
+            '5',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2'
+        ]
+    }
+];
 
 module.exports = friendsArray;
